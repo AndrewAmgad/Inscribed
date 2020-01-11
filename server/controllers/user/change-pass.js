35c49f746a2b86b59413618e9a6e4752bd87f030
Andrew Amgad <andrewamgad30@gmail.com>
2020-01-11 20:31:41 +0200

    Delete change-pass.js

--- server/controllers/user/change-pass.js
+++
@@ -1,9 +0,0 @@
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
-const User = require('../../models/user');
-
-module.exports = (req, res, next) => {
-    res.status(200).json({
-        message: "You've reached the change password route!"
-    })
-}
